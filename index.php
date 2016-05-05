b7eb51c6ff3fa02ca644690d61d4d38563e870f4
Gauglhofer <thomas.gauglhofer@vd.educanet2.ch>
2016-05-05 07:57:01 +0200

    Added files via upload

--- index.php
+++ index.php
@@ -15,71 +15,20 @@
         </span>
       </div>
       <div class="content_section floating_element">
-
-
-        <div class="section_header">
-          <div id="soumission"></div>
-          Télécharger depuis le serveur
-        </div>
-        <div class="content_section_text">
-          <p>
-              Ici, il y aura l'explication comment soumettre...<br>
-              <?php
-                echo "ça marche";
-              ?>
-          </p>
-
-        </div>
-        <div class="section_header">
-          <div id="soumission_fichiers_entiers"></div>
-                Verser un TE en entier (latex ou pdf)
-        </div>
-        <div class="content_section_text">
-          <p class="center">
-              <a href="soumission_latex_fichier.html">Fichier(s) latex à verser sur le serveur</a>&emsp;&emsp;&emsp;
-              <a href="soumission_pdf.html">Fichier pdf à verser sur le serveur</a>
-          </p>
-        </div>
-
-        <div class="section_header">
-            <div id="soumission_exo_latex"></div>
-                Verser un exercice (latex)
-        </div>
-        <div class="content_section_text">
-            <p>
-                Si le fichier latex était le suivant, il devrait pouvoir se compiler par pdflatex :
-            </p>
-            <pre>\documentclass[12pt,frenchb]{article}
-\usepackage{amsmath,amssymb,babel,multicol,enumerate,graphicx,tabularx,textcomp}
-\usepackage{pgf,tikz}
-\usetikzlibrary{shapes,positioning,arrows,patterns}
-\usepackage[T1]{fontenc}
-\usepackage[utf8]{inputenc}
-
-\newcommand{\ds}{\displaystyle}
-\newcommand{\C}{{\mathbb C}}
-\newcommand{\R}{{\mathbb R}}
-\newcommand{\Q}{{\mathbb Q}}
-\newcommand{\Z}{{\mathbb Z}}
-\newcommand{\N}{{\mathbb N}}
-
-\begin{document}
-    \begin{enumerate}
-	\item
-        {   </pre>
-            <p class="center">
-                <a href="soumission_latex_exo.html">Texte latex à verser sur le serveur</a>
-            </p>
-            <pre>
-        }
-    \end{enumerate}
-\end{document}</pre>
-            <p>
-                Remarque :<br>
-                Vous pouvez définir des commandes personnelles par "\newcommand" au début de votre texte.<br>
-                Comme celui-ci se trouve entre "{" et "}", ces commandes restent restreintes à votre exercice.
-            </p>
-        </div>
+      <?php
+          if ($_POST["userid"] == null) {$login_form=file_get_contents('login.html');
+              }
+          else { $login_form='';
+              }
+          echo $login_form;
+      ?>
+      <?php
+          if ($_POST["userid"] == 'gau') { $accueil=file_get_contents('accueil.html');
+              }
+          else { $accueil='Vous ne vous &ecirc;tes pas correctement identifié !';
+              }
+          echo $accueil;
+      ?>
       </div>
     </div>
   </body>
